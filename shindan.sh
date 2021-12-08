b948252a9730fbf7db2a175c74fc585447d8a84b
haruna <w10776e8w@yahoo.co.jp>
2021-12-09 03:10:58 +0900

    Update shindan.sh

--- shindan.sh
+++ shindan.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-_shindan() {
+function _shindan() {
     local url name status source inputs
     url="$1"
     name="$2"
@@ -15,7 +15,7 @@ _shindan() {
     inputs="$(
         echo -e "$source" | tr \< \\n | grep input |
         sed -nr '
-        s/value=""/value="'"$name"'"/
+        s/value=""/value="'$name'"/
         s/^.* name="([^"]+)".* value="([^"]+)">$/\1=\2/g
         4,6p' | tr \\n \& | sed 's/.$//'
     )"
@@ -25,16 +25,14 @@ _shindan() {
 
     # 連続で取得する場合はWaitを入れる
     # sleep "$[RANDOM%4+2]"
-
     rm cookie.txt
 }
 
-main(){
+function main(){
   if [ "$#" != 2 ]; then
-      echo "Usage: $0 <url> <name>" >&2
+      echo "usage: $0 <url> <name>" >&2
       return 1
   else
-
       _shindan "$1" "$2"
   fi
 }
