e90f48c003f2fcdd85aafcb84a09a0127bb2ea95
eggplants <w10776e8w@yahoo.co.jp>
2021-12-22 02:48:01 +0900

    add: user agent

--- shindan.sh
+++ shindan.sh
@@ -1,25 +1,28 @@
 #!/usr/bin/env bash
 
 function _shindan() {
-    local url name status source inputs
-    url="$1"
+    local url name status source_ inputs ua
+    url="https://shindanmaker.com/$1"
+    ua="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
     name="$2"
     status="$(
-        curl -LI "$url" -o /dev/null -w '%{http_code}' -s
+        curl -LI "$url" -o /dev/null -w '%{http_code}' -s -A "$ua"
     )"
     if [ "$status" != "200" ]; then
         echo "error: status code is invalid (${status})">&2
         return 1
     fi
-    source="$(curl -s -c cookie.txt "$url")"
+    source_="$(
+      curl -s -c cookie.txt "$url" -A "$ua"
+    )"
     inputs="$(
-        echo -e "$source" | tr \< \\n | grep input |
+        echo -e "$source_" | tr \< \\n | grep input |
         sed -nr '
         s/value=""/value="'$name'"/
         s/^.* name="([^"]+)".* value="([^"]+)">$/\1=\2/g
         4,6p' | tr \\n \& | sed 's/.$//'
     )"
-    curl -s -X POST -d "$inputs" -b cookie.txt "$url" |
+    curl -s -X POST -d "$inputs" -b cookie.txt "$url" -A "$ua" |
     grep -oE '<span id="shindanResult" [^>]+>(.*?)(</span>){5}' |
     sed -r 's_<br />_'\\n'_g;s/<[^>]+>//g'
 
@@ -29,8 +32,8 @@ function _shindan() {
 }
 
 function main(){
-  if [ "$#" != 2 ]; then
-      echo "usage: $0 <url> <name>" >&2
+  if [ "$#" != 2 ] || ! [[ "$1" =~ ^[1-9][0-9]*$ ]]; then
+      echo "usage: $0 <shindan id> <name>" >&2
       return 1
   else
       _shindan "$1" "$2"
--- shindan_cli/shindan.py
+++ shindan_cli/shindan.py
@@ -7,18 +7,23 @@ from bs4 import BeautifulSoup as BS  # type: ignore
 
 # type: (int, str, optional[bool]) -> str
 def shindan(page_id, shindan_name, wait=False):
+    headers = {
+        'User-Agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/96.0.4664.45 Safari/537.36')
+    }
     if page_id is int and page_id < 0:
         raise ValueError("invalid page id: %d" % page_id)
     url = 'https://shindanmaker.com/%d' % page_id
     session = requests.session()
-    s = session.get(url)
+    s = session.get(url, headers=headers)
     if s.status_code != 200:
         raise FileNotFoundError(s.status_code)
     source = BS(s.text, features="lxml")
     params = {i['name']: i['value']
               for i in source.find_all('input')[1:4]}
     params['shindanName'] = (shindan_name)
-    login = session.post(url, data=params)
+    login = session.post(url, data=params, headers=headers)
     if wait:
         time.sleep(random.uniform(2, 5))
     soup = BS(login.text, features="lxml").find('span', id='shindanResult')
