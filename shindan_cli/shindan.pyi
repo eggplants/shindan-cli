43c97f3916866532b97d18a0772edc33e5bd03b9
eggplants <w10776e8w@yahoo.co.jp>
2022-03-03 17:56:42 +0900

    add: typed marker

--- shindan_cli/shindan.pyi
+++
@@ -1,8 +0,0 @@
-from typing import List, Optional, TypedDict
-
-class ShindanResult(TypedDict):
-    results: List[str]
-    hashtags: List[str]
-    shindan_url: str
-
-def shindan(page_id: int, shindan_name: str, wait: Optional[bool] = ...) -> ShindanResult: ...
