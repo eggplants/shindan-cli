048efe9f86a08adaae1609e2b154c590b3fbe437
eggplants <w10776e8w@yahoo.co.jp>
2024-04-06 07:35:56 +0900

    v2.0.0

--- shindan_cli/__init__.py
+++ shindan_cli/__init__.py
@@ -4,5 +4,5 @@ from __future__ import annotations
 
 from .shindan import ShindanError, ShindanResult, shindan
 
-__version__ = "1.3.0"
+__version__ = "2.0.0"
 __all__ = ("shindan", "ShindanResult", "ShindanError")
